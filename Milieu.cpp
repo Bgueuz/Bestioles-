232889b9dae52d7f133d926a0063f98a30d0bd83
Bgueuz <36817367+Bgueuz@users.noreply.github.com>
2019-12-13 13:47:52 +0100

    Update Milieu.cpp

--- Milieu.cpp
+++ Milieu.cpp
@@ -54,3 +54,27 @@ int Milieu::nbVoisins( const Bestiole & b )
    return nb;
 
 }
+
+void Milieu::detection () {
+    for ( std::list<Bestiole>::iterator it = listeBestioles.begin() ; it != listeBestioles.end() ; ++it )
+    {
+        se = (*it).getSensors();
+        if (se != []) {
+                Voisins = (*it).getVoisins()
+                Detectes = (*it).getDetectes()
+                if (Voisins.empty=false){
+                        for ( std::list<Bestiole>::iterator ut = Voisins.begin() ; ut != listeVoisins.end() ; ++ut ) {
+                            if(((*it).vu(*ut)) ||((*it).entendu(*ut))) {
+                                Detectes.push_front(*ut);
+                                Voisins.remove(*ut); 
+                            }
+                        }
+                }
+        }
+        
+            
+            
+    }
+
+
+}
