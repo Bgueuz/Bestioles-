1b15dace0ca2b0d5d46bfe9ec6571e07bf63f378
Aly27 <38695039+Aly27@users.noreply.github.com>
2019-12-16 15:58:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+// if needed
+// #define _fileno fileno
+
+#include "CImg.h"
+
 #include "Aquarium.h"
 #include "Milieu.h"
-#include "Bestiole.h"
-
+#include "ConcreteBestiole.h"
 #include <iostream>
 
 using namespace std;
@@ -10,13 +14,41 @@ using namespace std;
 int main()
 {
 
-   Aquarium       ecosysteme( 640, 480, 30 );
+    Aquarium       ecosysteme( 640, 480, 30 );
 
-   for ( int i = 1; i <= 30; ++i )
-      ecosysteme.getMilieu().addMember( Bestiole() );
-   ecosysteme.run();
+    for ( int i = 1; i <= 20; ++i )
+    {
 
+        // Determining the bestiole's behavior at random
 
-   return 0;
+        int random_int = rand() % 100; // between 0 and 99
+        int random_behavior;
+        if (random_int < 20)
+            random_behavior = 1;
+        else if (random_int < 40)
+            random_behavior = 2;
+        else if (random_int < 60)
+            random_behavior = 3;
+        else if (random_int < 80)
+            random_behavior = 4;
+        else
+            random_behavior = 5;
+
+        // Determining whether or not the bestiole has a fin
+
+        // Determining whether or not the bestiole has an eye
+
+        // Determining whether or not the bestiole has an ear
+
+        // Determining whether or not the bestiole has camouflage
+
+        // Determining whether or not the bestiole has a shell
+
+        ecosysteme.getMilieu().addMember( ConcreteBestiole(random_behavior) );
+    }
+
+    ecosysteme.run();
+
+    return 0;
 
 }
