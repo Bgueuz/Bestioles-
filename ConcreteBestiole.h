845a770e7e3f2ddd4fd294d4112989c7fd8d631e
Bgueuz <36817367+Bgueuz@users.noreply.github.com>
2019-12-18 20:35:21 +0100

    Update ConcreteBestiole.h

--- ConcreteBestiole.h
+++ ConcreteBestiole.h
@@ -1,16 +1,16 @@
 #ifndef _CONCRETEBESTIOLES_H_
 #define _CONCRETEBESTIOLES_H_
 
+#include "Personality.h"
+#include "Bestiole.h"
+
 
 #include "UImg.h"
 #include <vector>
-#include "Bestiole.h"
-#include "Personality.h"
 #include <iostream>
 #include <list>
 
 using namespace std;
-
 
 class Milieu;
 
@@ -24,11 +24,6 @@ private :
     static const double     LIMITE_VUE;
     static const double     LIMITE_OUIE;
     static int              next;
-    bool                    schizophrene;
-    Personality*            personality;
-    int                dureedevie;
-    int                      vecu;
-    bool                      tue;
 
 private :
     int               identite;
@@ -36,8 +31,14 @@ private :
     double            cumulX, cumulY;
     double            orientation;
     double            vitesse;
-    bool              yeux;
-    bool              oreilles;
+    bool              schizophrene;
+    Personality*      personality;
+    int               type;
+    std::vector<float>      oreilles; // tableau : rayon du cercle de détection, probabilité de détection
+    std::vector<float>      yeux; // tableau : angle de vision, distance, probabilité
+
+
+private:
 
     T               * couleur;
     std::vector<ConcreteBestiole>   listeVoisinsOmni;
@@ -51,11 +52,10 @@ private :
 public :                                           // Forme canonique :
     ConcreteBestiole( void );                           // Constructeur par defaut
     ConcreteBestiole( const ConcreteBestiole & b );                 // Constructeur de copies
-    ~ConcreteBestiole( void );  
-                            // Destructeur
+    ~ConcreteBestiole( void );                              // Destructeur
     // Operateur d'affectation binaire par defaut
-    void Clonage1(const ConcreteBestiole & b, int xLim);
-    void Clonage2(const ConcreteBestiole & b, int yLim);
+    ConcreteBestiole Clonage1(int xLim);
+    ConcreteBestiole Clonage2(int yLim);
     void action( Milieu & monMilieu );
     void draw( UImg & support );
     list<ConcreteBestiole *> getVoisins();
@@ -69,28 +69,148 @@ public :                                           // Forme canonique :
     Personality* getPersonality();
     void setPersonality(Personality* newPersonality);
     void setSchizophrene(bool s);
+    void setType(int i);
+    int getType();
+    void setOreilles(float radius, float probability);
+    void setYeux(float angle, float radius, float probability);
 
     bool inRadiusVoisin(const ConcreteBestiole & b) const;
     bool checkCollision(const ConcreteBestiole & b) const;
 
-    int getX( void ) const { return x; };
-    int getY( void ) const { return y; };
-    double getVitesse( void ) const { return vitesse; };
-    double getOrientation( void ) const { return orientation; };;
-    std::vector<ConcreteBestiole> getVoisinsOmni( void ) const { return listeVoisinsOmni; };
+    int getX( void ) const
+    {
+        return x;
+    };
+    int getY( void ) const
+    {
+        return y;
+    };
+    double getVitesse( void ) const
+    {
+        return vitesse;
+    };
+    int getIdentite( void ) const
+    {
+        return identite;
+    };
+    double getOrientation( void ) const
+    {
+        return orientation;
+    };;
+    std::vector<ConcreteBestiole> getVoisinsOmni( void ) const
+    {
+        return listeVoisinsOmni;
+    };
+    std::vector<float> getOreilles()
+    {
+        return oreilles;
+    };
+    std::vector<float> getYeux()
+    {
+        return yeux;
+    };
+
+    void setVoisins(std::vector<ConcreteBestiole> listeVoisins)
+    {
+        listeVoisinsOmni = listeVoisins;
+    };
+    void setOrientation(double newOrientation)
+    {
+        orientation = newOrientation;
+    };
+    void setVitesse(double newVitesse)
+    {
+        vitesse = newVitesse;
+    };
+    void initCoords( int xLim, int yLim );
+    void initPersonality();
+    void randPersonality();
+    void initOreilles(Milieu* flotte);
+    void initYeux(Milieu* flotte);
+
+    friend bool operator==( const ConcreteBestiole & b1, const ConcreteBestiole & b2 );
+
+// Accessoires
+private :
+    // Camouflage
+    static double     max_cam;
+    static double     min_cam;
+    float camouflage = 0;
+
+    // Nageoire
+    static double     max_nage;
+    static double     min_nage;
+    float nageoire = 1.0;
+
+    // Carapace_dom
+    static double     max_cara_dom;
+    static double     min_cara_dom;
+    float carapaceDommage = 1.0;
+
+    // Carapace vitesse
+    static double     max_cara_vit;
+    static double     min_cara_vit;
+    float carapaceVitesse = 1.0;
+
+public :
+
+    void setAccesories(Milieu* flotte);
+    // Camouflage
+    void setCamouflage(float min_cam,float max_cam);
+    float getCamouflage()
+    {
+        return camouflage;
+    };
+
+    // Nageoire
+    void setNageoire(float min_nage,float max_nage);
+    float getNageoire()
+    {
+        return nageoire;
+    };
+
+    // Carapace Dommages
+    void setCarapaceDom(float min_cara_dom,float max_cara_dom);
+    float getCarapaceDom()
+    {
+        return carapaceDommage;
+    };
+
+    // Carapace Vitesse
+    void setCarapaceVit(float min_cara_vit,float max_cara_vit);
+    float getCarapaceVit()
+    {
+        return carapaceVitesse;
+    };
+
+    // Utile --> pourrait faire partie d'une interface
+    float RandomFloat(float a, float b);
+
+    // MORT
+
+private :
+
+    int                dureedevie = std::rand() % 100;
+    int                      vecu = 0;
+    bool                      tue = false;
+
+public :
+
     bool getTue( void ) const { return tue; };
     int getID( void ) const { return identite; };
     int getVecu( void ) const { return vecu; };
     int getDureeDeVie( void ) const { return dureedevie; };
-
     void Kill(void);
-  
-    void setVoisins(std::vector<ConcreteBestiole> listeVoisins){listeVoisinsOmni = listeVoisins;};
-    void setOrientation(double newOrientation){orientation = newOrientation;};
     void vie();
-    void initCoords( int xLim, int yLim );
 
-    friend bool operator==( const ConcreteBestiole & b1, const ConcreteBestiole & b2 );
+//Clonage
+
+public :
+
+    void InitCoordsClone( int xclone, int yclone);
+   // int getX( void ) const { return x; };
+    //int getY( void ) const { return y; };
+
 
 };
 
