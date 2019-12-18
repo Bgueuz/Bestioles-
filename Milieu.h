a0a9126e15088d2cd30b31120ec37eb99d1c2c5a
Bgueuz <36817367+Bgueuz@users.noreply.github.com>
2019-12-18 16:12:12 +0100

    Update Milieu.h

--- Milieu.h
+++ Milieu.h
@@ -10,6 +10,23 @@
 using namespace std;
 
 
+#ifndef _MILIEU_H_
+#define _MILIEU_H_
+
+
+#include "UImg.h"
+#include "ConcreteBestiole.h"
+#include "GregairePersonality.h"
+#include "PeureusePersonality.h"
+#include "KamikazePersonality.h"
+#include "PrevoyantePersonality.h"
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
 class Milieu : public UImg
 {
 
@@ -39,112 +56,78 @@ public :
     {
         listeBestioles.push_back(b);
         listeBestioles.back().initCoords(width, height);
-        listeBestioles.back().initPersonality();
 
-        // Ajout de capteurs
-        listeBestioles.back().initOreilles(this);
-        listeBestioles.back().initYeux(this);
+        // Determining the bestiole's behavior at random
 
-        // Accessoires
-        listeBestioles.back().setAccesories(this);
+        int random_int = std::rand() % 100; // between 0 and 99
+        int random_behavior;
+        if (random_int < 20)
+            random_behavior = 1;
+        else if (random_int < 40)
+            random_behavior = 2;
+        else if (random_int < 60)
+            random_behavior = 3;
+        else if (random_int < 80)
+            random_behavior = 4;
+        else
+            random_behavior = 5;
 
-        cout << "bestiole :" << listeBestioles.back().getIdentite() << endl;
-        cout << "   cette bestiole à un camouflage de " << listeBestioles.back().getCamouflage() << endl;
-        cout << "   cette bestiole à des nageoires de " << listeBestioles.back().getNageoire() << endl;
-        cout << "   cette bestiole à une carapace " << endl;
-        cout << "       qui réduit les dommages de  " << listeBestioles.back().getCarapaceDom() << endl;
-        cout << "       qui réduit la vitesse de  " << listeBestioles.back().getCarapaceVit() << endl;
+        switch (random_behavior)
+        {
+        case 1: // grégaire
+        {
+            listeBestioles.back().setSchizophrene(false);
+            listeBestioles.back().setPersonality(new GregairePersonality());
+        }
+        break;
+        case 2: // peureuse
+        {
+            listeBestioles.back().setSchizophrene(false);
+            listeBestioles.back().setPersonality(new PeureusePersonality());
+        }
+        break;
+        case 3: // kamikaze
+        {
+            listeBestioles.back().setSchizophrene(false);
+            listeBestioles.back().setPersonality(new KamikazePersonality());
+        }
+        break;
+        case 4: // prévoyante
+        {
+            listeBestioles.back().setSchizophrene(false);
+            listeBestioles.back().setPersonality(new PrevoyantePersonality());
+        }
+        break;
+        case 5: // personnalités multiples
+        {
+            listeBestioles.back().setSchizophrene(true);
+            listeBestioles.back().setPersonality(NULL);
+        }
+        break;
+
+        default:
+            listeBestioles.back().setSchizophrene(false);
+            listeBestioles.back().setPersonality(new GregairePersonality());
+        }
+
+        // Determining whether or not the bestiole has a fin
+
+        // Determining whether or not the bestiole has an eye
+
+        // Determining whether or not the bestiole has an ear
+
+        // Determining whether or not the bestiole has camouflage
+
+        // Determining whether or not the bestiole has a shell
 
     }
-
     int nbVoisins( const ConcreteBestiole & b );
     void detection ();
     void updateVoisins(ConcreteBestiole & b);
     void collisionsAll();
-
-public:
-
-    //Accesoires
-
-        // Camouflage
-    void setCamouflageLimits(float max_cam, float min_cam);
-    float getMaxCam(){return MAX_CAMO;};
-    float getMinCam(){return MIN_CAMO;};
-
-        // Nageoire
-    void setNageoireLimits(float max_nage, float min_nage);
-    float getMaxNage(){return MAX_NAGE;};
-    float getMinNage(){return MIN_NAGE;};
-
-        // Carapace - Dommage
-    void setCaraDomLimits(float max_cara_dom, float min_cara_dom);
-    float getMaxCaraDom(){return MAX_CARA_DOM;};
-    float getMinCaraDom(){return MIN_CARA_DOM;};
-
-        // Carapace - Vitesse
-    void setCaraVitLimits(float max_cara_vit, float min_cara_vit);
-    float getMaxCaraVit(){return MAX_CARA_VIT;};
-    float getMinCaraVit(){return MIN_CARA_VIT;};
-
-    // Capteurs
-
-    void setEarRadiusLimits(float max_radius, float min_radius);
-    float getMaxEarRadius(){return MAX_EAR_RADIUS;};
-    float getMinEarRadius(){return MIN_EAR_RADIUS;};
-
-    void setEarProbabilityLimits(float max_probability, float min_probability);
-    float getMaxEarProbability(){return MAX_EAR_PROBABILITY;};
-    float getMinEarProbability(){return MIN_EAR_PROBABILITY;};
-
-    void setEyeAngleLimits(float max_angle, float min_angle);
-    float getMaxEyeAngle(){return MAX_EYE_ANGLE;};
-    float getMinEyeAngle(){return MIN_EYE_ANGLE;};
-
-    void setEyeRadiusLimits(float max_radius, float min_radius);
-    float getMaxEyeRadius(){return MAX_EYE_RADIUS;};
-    float getMinEyeRadius(){return MIN_EYE_RADIUS;};
-
-    void setEyeProbabilityLimits(float max_probability, float min_probability);
-    float getMaxEyeProbability(){return MAX_EYE_PROBABILITY;};
-    float getMinEyeProbability(){return MIN_EYE_PROBABILITY;};
-
-
-private:
-    // Camouflage
-    float MAX_CAMO = 1.0;
-    float MIN_CAMO = 0.0;
-
-    // Nageoire
-    float MAX_NAGE = 2.0;
-    float MIN_NAGE = 1.0;
-
-    // Carapace
-        // Reduction Dommages subit / chances de mourir à l'impact
-    float MAX_CARA_DOM = 4.0;
-    float MIN_CARA_DOM = 1.0;
-
-            // Reduction Vitesse
-    float MAX_CARA_VIT = 3.0;
-    float MIN_CARA_VIT = 1.0;
-
-    // Oreille
-    float MIN_EAR_RADIUS = 100;
-    float MAX_EAR_RADIUS = 200;
-
-    float MIN_EAR_PROBABILITY = 0;
-    float MAX_EAR_PROBABILITY = 1;
-
-    // Yeux
-    float MIN_EYE_ANGLE = 0;
-    float MAX_EYE_ANGLE = M_PI;
-
-    float MIN_EYE_RADIUS = 50;
-    float MAX_EYE_RADIUS = 100;
-
-    float MIN_EYE_PROBABILITY = 0;
-    float MAX_EYE_PROBABILITY = 1;
-
-
+    void gestionvie();
+    //void funTest();
+    //void killBestiole(ConcreteBestiole &bestiole);
 };
 
 
